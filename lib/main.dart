bf923f912ef66126e013a67b4f0d4d8f87e3ae3c
ntsts03 <ntsts03@gmail.com>
2024-09-03 17:18:04 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:instaclone/pages/feed_page.dart';
+import 'package:instaclone/pages/my_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final _pageWidgets = [
+    FeedPage(),
+    MyPage(),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageWidgets.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
+
+ class InstagramPostItem extends StatelessWidget {
+   const InstagramPostItem({Key? key, required this.imageUrl}) : super(key: key);
+ 
+   final String imageUrl;
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Image.network(
+       imageUrl,
+       fit: BoxFit.cover,
+     );
+   }
+ }
---
+++ lib/pages/my_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class MyPage extends StatelessWidget {
+  MyPage({Key? key}) : super(key: key);
+
+  final images = [
+    'https://img.icons8.com/?size=100&id=32323&format=png&color=000000',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvoSuexQoraIJ3Xc8Qde7Y_A-7v7vfwWxveA&usqp=CAU',
+    'https://img.icons8.com/?size=100&id=32323&format=png&color=000000',
+    'https://news.value-press.com/wp-content/uploads/interview_top_image_pakutaso.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('マイページ')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Image.network(
+                    'https://img.icons8.com/?size=100&id=32323&format=png&color=000000',
+                    width: 60,
+                    height: 60,
+                  ),
+                  Spacer(),
+                  const SizedBox(width: 16),
+                  Column(
+                    children: [
+                      Text(
+                        '7,041',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('投稿'),
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                  Column(
+                    children: [
+                      Text(
+                        '4.6億',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('フォロワー'),
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                  Column(
+                    children: [
+                      Text(
+                        '99',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('フォロー'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            GridView.count(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 3,
+              children: images.map((imageUrl) {
+                return InstagramPostItem(imageUrl: imageUrl);
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
